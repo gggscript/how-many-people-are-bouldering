7f6d47f8a0b19c8a1df71242aeb700004bdbdff3
gggscript <gggscript@gmail.com>
2022-12-03 20:20:48 +0100

    Introduce interface

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
+// List of Gyms
 interface Gym {
   name: string
   website: string
   api: GymHeadcountAPI
 }
 
+// Different APIs
 type GymHeadcountAPI = BoulderadoAPI | WebclimberAPI
-
 interface BoulderadoAPI {
   type: 'boulderado'
   token: string
 }
-
 interface WebclimberAPI {
   type: 'webclimber'
   id: number
@@ -18,14 +18,23 @@ interface WebclimberAPI {
 }
 
 type APIResult = RelativeResult | AbsoluteResult
-
 interface RelativeResult {
   type: 'relative'
   percentage: number
 }
-
 interface AbsoluteResult {
   type: 'absolute'
   current: number
   max: number
 }
+
+// TODO: Load from JSON file
+const gyms: readonly Gym[] = []
+
+async function getOccupancy(gym: Gym): Promise<APIResult> {
+  // TODO: Implement Boulderado
+  // TODO: Implement Webclimber
+  throw new Error('Gym Headcount API not implemented')
+}
+
+export { gyms, getOccupancy }
